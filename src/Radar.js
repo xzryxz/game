9c087cc9ccd73c3f0025e75c88b1f619618675a7
Love Kempe <love.kempe@gmail.com>
2016-09-02 01:11:24 +0200

    Added arrow key support.

--- src/Radar.js
+++ src/Radar.js
@@ -3,6 +3,37 @@ import RadarUi from './RadarUi.js';
 
 export default class Radar extends Component {
   render() {
+
+    document.onkeydown = checkKey.bind(this);
+
+    function checkKey (e) {
+      const dest = this.props.dest
+      if (e.keyCode === '38') {
+        this.props.setDest({
+          x: (dest.x ? dest.x : dest.x),
+          y: (dest.y ? dest.y : dest.y) - 1,
+        })
+      }
+      else if (e.keyCode === '40') {
+        this.props.setDest({
+          x: (dest.x ? dest.x : dest.x),
+          y: (dest.y ? dest.y : dest.y) + 1,
+        })
+      }
+      else if (e.keyCode === '37') {
+        this.props.setDest({
+          x: (dest.x ? dest.x : dest.x) - 1,
+          y: (dest.y ? dest.y : dest.y),
+        })
+      }
+      else if (e.keyCode === '39') {
+        this.props.setDest({
+          x: (dest.x ? dest.x : dest.x) + 1,
+          y: (dest.y ? dest.y : dest.y),
+        })
+      }
+    }
+
     return (
       <div className="Radar">
         <RadarUi
